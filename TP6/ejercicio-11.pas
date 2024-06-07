c0ad5df1a4368c0cc953144309f8f01144b3ba6f
Ernesto Ardenghi <eardenghi@gmail.com>
2024-06-07 17:38:20 -0300

    Actualización

---
+++ TP6/ejercicio-11.pas
@@ -0,0 +1,11 @@
+{
+  La Facultad de Informática debe seleccionar los 10 egresados con mejor promedio a los que la UNLP les
+  entregará el premio Joaquín V. González. De cada egresado se conoce su número de alumno, apellido y
+  el promedio obtenido durante toda su carrera.
+  
+  Implementar un programa que:
+  a. Lea la información de todos los egresados, hasta ingresar el código 0, el cual no debe procesarse.
+  b. Una vez ingresada la información de los egresados, informe el apellido y número de alumno de los
+  egresados que recibirán el premio. La información debe imprimirse ordenada según el promedio
+  del egresado (de mayor a menor).
+}
